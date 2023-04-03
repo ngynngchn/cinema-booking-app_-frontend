f3532c4ac7bc5bc776b930f2dddeca5d013d6a79
Hanni <ngynngchn@gmail.com>
2023-04-03 16:45:40 +0200

    Need to deploy and add styles

--- src/pages/MovieHall.jsx
+++ src/pages/MovieHall.jsx
@@ -4,6 +4,8 @@ import Seat from "../components/Seat.jsx";
 import { v4 as uuid4 } from "uuid";
 import "./MovieHall.css";
 
+// TODO: After booking the tickets, it should display something like : Thank you for booking the tickets! Enjoy your movie and redirect back to /home
+
 function MovieHall() {
 	const [seats, setSeats] = useState([]);
 	const [selection, setSelected] = useState([]);
