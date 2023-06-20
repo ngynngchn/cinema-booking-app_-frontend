605960d5e5b59a95c6651f3e8f5fa26b6a95f17f
ngynngchn <ngynngchn@gmail.com>
2023-06-20 11:17:07 +0200

    used styled components for styling

--- src/components/Seat.jsx
+++ src/components/Seat.jsx
@@ -1,17 +1,40 @@
 import "./Seat.css";
+import styled from "styled-components";
 
 function Seat({ data, active, onClick }) {
 	return (
-		<button
+		<SeatElement
 			title={data.reserved ? "reserved" : `Seat ${data.id}`}
-			type="button"
-			className={`Seat ${data.reserved ? "reserved" : ""} ${
-				active ? "selected " : ""
-			}`}
 			disabled={data.reserved}
+			status={data.reserved ? "reserved" : active ? "selected" : ""}
 			onClick={onClick}
-			value={data.id}></button>
+			value={data.id}
+		/>
 	);
 }
 
 export default Seat;
+
+const SeatElement = styled.button`
+	all: unset;
+	width: 25px;
+	height: 25px;
+	border-radius: 4px;
+	cursor: pointer;
+	background: ${({ status }) => {
+		switch (status) {
+			case "reserved":
+				return "linear-gradient(145deg, #1d1d1d, #181818)";
+			case "selected":
+				return "linear-gradient(145deg, #e84849, #c33c3d)";
+			case "free":
+				return "linear-gradient(145deg, #f2f2f2, #e6e6e6)";
+			default:
+				return "linear-gradient(145deg, #f2f2f2, #e6e6e6)";
+		}
+	}};
+
+	&:not(:disabled):hover {
+		background: linear-gradient(145deg, #e84849, #c33c3d);
+	}
+`;
